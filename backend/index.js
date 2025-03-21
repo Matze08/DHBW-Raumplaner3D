642cda448bcf5b7c27f3e34d4e9830179b239640
Michael B <76816166+WomboCombo53@users.noreply.github.com>
2025-03-21 15:40:51 +0100

    include backend info message

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+console.log("INFO | Starting Raumplaner-Backend...")
 const app = express()
 const port = 3187
 
@@ -8,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`INFO | Raumplaner-Backend listening on port ${port}`)
 })
